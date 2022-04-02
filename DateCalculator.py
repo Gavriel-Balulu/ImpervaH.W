d6c29e0bf1b6ef599f93a69e55bffec268ce70a2
Gavriel-Balulu <58724459+Gavriel-Balulu@users.noreply.github.com>
2022-04-02 22:16:06 +0300

    Update DateCalculator.py

--- DateCalculator.py
+++ DateCalculator.py
@@ -1,8 +1,8 @@
 days_in_months = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31,
-                      9: 30, 10: 31, 11: 30, 12: 31}
+                  9: 30, 10: 31, 11: 30, 12: 31}
 
 
-def GetDateFromUser():
+def get_date_from_user():
     print('Input a date like the following format, DD.MM.YYYY')
     current_date = input()
 
@@ -13,28 +13,27 @@ def GetDateFromUser():
     return current_year, current_month, current_day
 
 
-def GetDeltaFromUser():
+def get_days_number_from_user():
     print('Input a number of days to add')
     days_number = int(input())
     return days_number
 
 
-def GetDaysInMonth(current_month, current_year):
+def get_days_in_month(current_month, current_year):
     if current_month == 2 and (current_year % 4 == 0 and current_year % 100 == 0 and current_year % 400 == 0):
         return 29
     else:
         return days_in_months[current_month]
 
 
-def ValidateInput(current_year, current_month, current_day, days_number):
+def validate_input(current_year, current_month, current_day, days_number):
     if current_month not in days_in_months.keys():
-        raise ValueError("month must be in 1..12 {}".format(current_month))
-    if current_day > GetDaysInMonth(current_month, current_year) or current_day < 1:
-        raise ValueError("day is out of range for month {}".format(current_day))
+        raise ValueError(" {} is out of range - input month between 1 - 12 ".format(current_month))
+    if current_day > get_days_in_month(current_month, current_year) or current_day < 1:
+        raise ValueError("day {} is out of range for month ".format(current_day))
 
 
-
-def JumpToNextMonth(current_month, current_year):
+def jump_to_next_month(current_month, current_year):
     if current_month == 12:
         current_month = 1
         current_year += 1
@@ -43,7 +42,7 @@ def JumpToNextMonth(current_month, current_year):
     return current_month, current_year
 
 
-def JumpToPrevMonth(current_month, current_year):
+def jump_to_prev_month(current_month, current_year):
     if current_month == 1:
         current_month = 12
         current_year -= 1
@@ -51,26 +50,28 @@ def JumpToPrevMonth(current_month, current_year):
         current_month -= 1
     return current_month, current_year
 
-def DayNumberInYear(current_day, current_month, current_year):
-    month=1
-    DayInYear = 0  
-    while month < current_month:
-        DayInYear += GetDaysInMonth(month, current_year)
-        month +=1
-    DayInYear += current_day
-    return DayInYear
 
-def GetDaysInYear(yearNumber):
-    if yearNumber % 4 == 0 and yearNumber % 100 == 0 and yearNumber % 400 == 0:
+def day_number_in_year(current_day, current_month, current_year):
+    month=1
+    day_in_year = 0
+    while month < current_month:
+        day_in_year += get_days_in_month(month, current_year)
+        month += 1
+    day_in_year += current_day
+    return day_in_year
+
+
+def get_days_in_year(year_number):
+    if year_number % 4 == 0 and year_number % 100 == 0 and year_number % 400 == 0:
         return 366
     return 365
 
 
 try:
-    current_year, current_month, current_day = GetDateFromUser()
-    days_number = GetDeltaFromUser()
+    current_year, current_month, current_day = get_date_from_user()
+    days_number = get_days_number_from_user()
     try:
-        ValidateInput(current_year, current_month, current_day, days_number)
+        validate_input(current_year, current_month, current_day, days_number)
     except ValueError as error:
         print(error)
         exit()
@@ -78,18 +79,17 @@ except BaseException as e:
     print("Wrong input:{}".format(e))
     exit()
 
-
 if days_number > 0:
     days_left = days_number
-    while DayNumberInYear(current_day, current_month, current_year) + days_left > GetDaysInYear(current_year):
-        days_left -= GetDaysInYear(current_year) - DayNumberInYear(current_day, current_month, current_year)
+    while day_number_in_year(current_day, current_month, current_year) + days_left > get_days_in_year(current_year):
+        days_left -= get_days_in_year(current_year) - day_number_in_year(current_day, current_month, current_year)
         current_year += 1
         current_month = 1
         current_day = 0
 
-    while current_day + days_left > GetDaysInMonth(current_month, current_year):
-        days_left -= GetDaysInMonth(current_month, current_year) - current_day
-        current_month, current_year = JumpToNextMonth(current_month, current_year)
+    while current_day + days_left > get_days_in_month(current_month, current_year):
+        days_left -= get_days_in_month(current_month, current_year) - current_day
+        current_month, current_year = jump_to_next_month(current_month, current_year)
         current_day = 0
 
     current_day += days_left
@@ -104,11 +104,11 @@ else:
 
     while current_day - days_left <= 0:
         days_left -= current_day
-        current_month, current_year = JumpToPrevMonth(current_month, current_year)
-        current_day = GetDaysInMonth(current_month, current_year)
+        current_month, current_year = jump_to_prev_month(current_month, current_year)
+        current_day = get_days_in_month(current_month, current_year)
     current_day -= days_left
 
-print("Target date is {}.{}.{}".format(current_day,current_month,current_year))
+print("Target date is {}.{}.{}".format(current_day, current_month, current_year))
 
 
 
